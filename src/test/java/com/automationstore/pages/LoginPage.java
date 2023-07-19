b92b7805472145bbeac4bdf63c364ac2e100e910
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2023-07-19 09:57:12 +0530

    Update LoginPage.java

--- src/test/java/com/automationstore/pages/LoginPage.java
+++ src/test/java/com/automationstore/pages/LoginPage.java
@@ -18,44 +18,20 @@ public class LoginPage extends BaseClass {
 		this.driver = driver;
 		PageFactory.initElements(driver,this);
 	}
+	
+	@FindBy(xpath="//ul[@class='info_links_footer']/li[7]/a")
+	private WebElement linklogin;
 
-	@FindBy(xpath="//div[@class='form-group']//input[@id='loginFrm_loginname']")
+	@FindBy(xpath="//input[@id='loginFrm_loginname']")
+	//@FindBy(id="loginFrm_loginname")
 	private WebElement txtboxUsername;
 	
 	@FindBy(xpath="//div[@class='form-group']//input[@id='loginFrm_password']")
 	private WebElement txtboxpassword;
 	
-	/*@FindBy(xpath="//form[@id='loginFrm']/fieldset/button")
-	private WebElement loginButton;*/
-
-	@FindBy(xpath="//ul[@class='info_links_footer']/li[7]/a")
+	@FindBy(xpath="//form[@id='loginFrm']/fieldset/button")
 	private WebElement loginButton;
 	
-	public void clickOnUsernameTextField() {
-		txtElementClick(txtboxUsername);
-	}
-	
-	public void enterUsername(String uid) {
-		enterText(txtboxUsername,uid);
-	}
-	
-	
-	public void clickOnPasswordTextField() {
-		txtElementClick(txtboxpassword);
-	}
-	
-	public void enterPassword(String pwd) {
-		enterText(txtboxpassword,pwd);
-	}
-
-	public void clickOnLoginButton() {
-		btnClick(loginButton);
-	}
-	
-	public void getLoginPageTitle() {
-		getTitle(driver);
-	}
-
 	public void navigateToLogin() throws InterruptedException {
 		int count =0;
 		try{
@@ -66,19 +42,67 @@ public class LoginPage extends BaseClass {
 				Thread.sleep(3000);
 				count++;
 			}
-			if(loginButton.isDisplayed()){
+			if(linklogin.isDisplayed()){
 				System.out.println("Login button found");
-				loginButton.click();
+				linklogin.click();
 			}
 
 		}catch (NoSuchElementException e){
 			System.out.println("Inside catch block with exception:- "+e.getMessage());
+			//Actions act = new Actions(driver);
+			//act.sendKeys(Keys.PAGE_DOWN).build().perform(); //Page Down
+			//System.out.println("Scroll down perfomed");
+			//Thread.sleep(3000);
+			//linklogin.click();
+		}
+	}
+
+	
+	
+	public void scrollToUsernameTextField() throws InterruptedException {
+		try {
 			Actions act = new Actions(driver);
 			act.sendKeys(Keys.PAGE_DOWN).build().perform(); //Page Down
 			System.out.println("Scroll down perfomed");
 			Thread.sleep(3000);
-			loginButton.click();
+			if(txtboxUsername.isDisplayed()){
+				System.out.println("Textbox username found");
+				txtElementClick(txtboxUsername);
+			}
+		}catch (NoSuchElementException e){
+		System.out.println("Inside catch block with exception:- "+e.getMessage());
+			}
 		}
+	
+	public void enterUsername(String uid) {
+		enterText(txtboxUsername,uid);
+	}
+	
+	
+	public void scrollToPasswordTextField() throws InterruptedException {
+		try {
+			Actions act = new Actions(driver);
+			act.sendKeys(Keys.PAGE_DOWN).build().perform(); //Page Down
+			System.out.println("Scroll down perfomed");
+			Thread.sleep(3000);
+			if(txtboxpassword.isDisplayed()){
+				System.out.println("Textbox password found");
+				txtElementClick(txtboxpassword);
+				}
+			}catch (NoSuchElementException e){
+		System.out.println("Inside catch block with exception:- "+e.getMessage());
+			}
+	}
+	
+	public void enterPassword(String pwd) {
+		enterText(txtboxpassword,pwd);
 	}
 
+	public void clickOnLoginButton() {
+		btnClick(loginButton);
+	}
+	
+	public String getLoginPageTitle() {
+		return getTitle(driver);
+	}
 }
