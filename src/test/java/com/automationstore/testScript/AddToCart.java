22b38f502863a8403332964aba8551f838191d11
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2023-07-23 12:30:49 +0530

    Update AddToCart.java

--- src/test/java/com/automationstore/testScript/AddToCart.java
+++ src/test/java/com/automationstore/testScript/AddToCart.java
@@ -1,5 +1,30 @@
 package com.automationstore.testScript;
 
-public class AddToCart {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
+import com.automationstore.pages.LoginPage;
+import com.automationstore.pages.ShoppingCart;
+import com.automationteststore.base.BaseClass;
+
+public class AddToCart extends BaseClass{
+	WebDriver driver;
+			
+	@Test
+	public void testAddProductToCart() {
+		try {
+			driver=BaseClass.launchApp();
+			Login login = new Login();
+			login.testLogin();
+			ShoppingCart sp = new ShoppingCart(driver);
+			Thread.sleep(1000);
+			sp.moveMouseOnApparelLinkMenu();
+			Thread.sleep(1000);
+			sp.moveMouseTShirtLinkMenu();
+			sp.getPageTitle();
+			sp.clickOnRandomProduct();
+			}	catch(Exception e) {
+			System.out.println(e.getMessage());
+			}
+		}
 }
